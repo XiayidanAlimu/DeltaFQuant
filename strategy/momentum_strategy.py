2af374c5f2042d95aa9598d90cf0f662398e9213
xiayidan.alimu <xiayidan.alimu@gmail.com>
2026-02-12 16:03:25 +0800

    5-5 动量策略：生成交易信号

--- strategy/momentum_strategy.py
+++ strategy/momentum_strategy.py
@@ -20,25 +20,57 @@ def get_data(start_date, end_date, type, use_cols, index_symbol='000300.XSHG'):
     stocks = st.get_index_list(index_symbol)
     data_concat = pd.DataFrame()
     # 获取股票数据
-    for code in stocks[0:9]:
+    for code in stocks[0:5]:
         data = st.get_csv_price(code, start_date, end_date, type, use_cols)
         data.columns = [code]
         data_concat = pd.concat([data_concat, data], axis=1)
     return data_concat
 
-def momentum(data_concat, shift_n=1):
+def momentum(data_concat, shift_n=1, top_n=2):
     """
 
     :param data_concat: dataFrame
     :param shift_n: 业绩统计周期，单元：月
+    :param top_n: int 获取极值的个数
     :return:
     """
     data_concat.index = pd.to_datetime(data_concat.index)
+    # 转换时间频率：日 =》 月
     data_month = data_concat.resample('ME').last()
     # 计算过去n (shift_n) 个月的收益率
     print(data_month)
+    # 收益率 = 期末值/期初值 - 1
     shift_return = data_month / data_month.shift(shift_n) - 1
     print(shift_return)
+    buy_signals = get_top_stocks(shift_return, top_n)
+    sell_signals = get_top_stocks(-1*shift_return, top_n)
+    signals = buy_signals - sell_signals
+    print(signals)
+
+    # 生成交易信号：
+    # 收益率排前n 》 赢家组合 》 买入信号：1
+    # 收益率排后n 》 输家组合 》 卖出信号：-1
+
+def get_top_stocks(data, top_n):
+    """
+    找到前n位的极值，并转换为信号返回
+    :param data:
+    :param top_n: int 获取极值的个数
+    :return: signals:df, 返回0,1,-1信号数据表
+    """
+    signals = pd.DataFrame(index=data.index, columns=data.columns)
+    # 对data的每一行进行遍历，找到里面的最大值，并利用bool函数标注0或者1或者-1信号
+    for index, row in data.iterrows():
+        # index 日期
+        # row 每一只股票的收益率
+        # print(index)
+        # print(row)
+        largest = row.nlargest(top_n)
+        # print(largest)
+        signals.loc[index] = row.isin(largest).astype(int)
+    return signals
+
+
 
 if __name__ == '__main__':
     data = get_data('2024-06-10', '2025-04-01', 'price', ['date', 'close'])
